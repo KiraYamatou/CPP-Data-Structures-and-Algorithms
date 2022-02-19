e6bb6581a9bc233d2057e16859da174bf5da1c40
Erik <janerikgrajo@gmail.com>
2022-02-18 19:14:54 -0800

    Update Binary Search.cpp

--- Search Algorithms/Binary Search.cpp
+++ Search Algorithms/Binary Search.cpp
@@ -1,48 +1,49 @@
-/// Exponential Search Algorithm
+/// Binary Search Algorithm
 /// C++ 
 #include <iostream>
 
 
 using namespace std;
 
-int binarySearch(int arr[], int, int, int);
+int binarySearch(const int [], int, int);
 
-int exponentialSearch(int arrp[], int n, int x) {
-	if (arr[0] == x) {
-		return 0;
-	}
-	int i = 1;
-	while (i < n && arr[i] <= x) {
-		i = i * 2;
-	}
-	return binarySearch(arr, i / 2, min(i, n - 1), x);
-}
+const int SIZE = 5;
 
-int binarySearch(int arrp[], int l, int r, int x) {
-	if (r >= 1) {
-		int mid = 1 + (r - 1) / 2;
+int binarySearch(const int array[], int size, int value) {
+	int first = 0,
+		last = size - 1,
+		middle,
+		position = -1;
+	bool found = false;
 
-		if (arr[mid] == x) {
-			return mid;
+	while (!found && first <= last) {
+		middle = (first + last) / 2;
+		if (array[middle] == value) {
+			found = true;
+			position = middle;
 		}
-		if (arr[mid] > x) {
-			return binarySearch(arr, l, mid - 1, x);
+		else if (array[middle] > value) {
+			last = middle - 1;
 		}
-		return binarySearch(arr, mid + 1, r, x);
+		else {
+			first = middle + 1;
+		}
 	}
-
-	return -1;
+	return position;
 }
 
 int main()
 {
-	int arr[] = { 2, 3, 4, 10, 40 };
-	int n = sizeof(arr) / sizeof(arr[0]);
-	int x = 40;
-	int result = exponentialSearch(arr, n, x);
-	(result == -1)
-		? cout << "Element is not present in the array." << endl
-	: cout << "Element is presnet at index " << result << endl;
-	return 0;
-}
+	int ar[SIZE] = { 2, 3, 4, 10, 40};
+	int x = 10;
+	int results;
 
+	results = binarySearch(ar, SIZE, x);
+
+	if (results == -1) {
+		cout << "That number does not exist within the array" << endl;
+	}
+	else {
+		cout << "That number is found at element " << results << " in the array." << endl;
+	}
+}
