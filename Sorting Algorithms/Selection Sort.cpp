26ddb8001ee083f7a7e185d2190428415d83de22
Erik <janerikgrajo@gmail.com>
2022-02-22 23:00:17 -0800

    Add files via upload

---
+++ Sorting Algorithms/Selection Sort.cpp
@@ -0,0 +1,53 @@
+/// Selection Sort
+/// C++ 
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+void selectionSort(int [], int);
+void showArray(const int [], int);
+
+void selectionSort(int array[], int size) {
+	int startScan, minIndex, minValue;
+
+	
+
+	for (startScan = 0; startScan < (size - 1); startScan++) {
+		minIndex = startScan;
+		minValue = array[startScan];
+
+		for (int index = startScan + 1; index < size; index++) {
+			if (array[index] < minValue) {
+				minValue = array[index];
+				minIndex = index;
+			}
+		}
+		array[minIndex] = array[startScan];
+		array[startScan] = minValue;
+	}
+
+}
+
+void showArray(const int array[], int size) {
+	for (int count = 0; count < size; count++) {
+		cout << array[count] << " ";
+	}
+}
+int main()
+{
+	const int SIZE = 6;
+	int values[SIZE] = { 5, 7, 2, 8, 9, 1 };
+
+	cout << "The unsorted values are: ";
+	showArray(values, SIZE);
+	cout << endl;
+
+	selectionSort(values, SIZE);
+
+	cout << "The sorted values are: ";
+	showArray(values, SIZE);
+
+	return 0;
+}
+
